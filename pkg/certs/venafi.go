7e530bc5aba798baf7f18bb73536695651667227
Gl4di4torRr <gl4di4torrr@gmail.com>
2019-02-25 16:13:45 -0500

    initial conversion to new vcert library

--- pkg/certs/venafi.go
+++ pkg/certs/venafi.go
@@ -1,26 +1,25 @@
 package certs
 
 import (
-	"fmt"
-	"os"
+	"crypto/tls"
+	"crypto/x509/pkix"
+	"github.com/Venafi/vcert"
+	"github.com/Venafi/vcert/pkg/certificate"
+	t "log"
+	"net/http"
 	"time"
-
-	"github.com/Venafi/govcert"
-	vcert "github.com/Venafi/govcert/embedded"
-	"github.com/hashicorp/terraform/helper/resource"
-	"github.com/sirupsen/logrus"
 )
 
 type VenafiProvider struct {
 }
 
 func (p *VenafiProvider) Provision(host string, validFrom string, validFor time.Duration, isCA bool, rsaBits int, ecdsaCurve string) (keypair KeyPair, certError error) {
+	
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 
 	if len(host) == 0 {
 		return KeyPair{}, NewErrBadHost("host cannot be empty")
 	}
-	logrus.Infof("Creating Venafi certificate for :" + host)
-	logrus.Infof("Venafi Zone: " + os.Getenv("VENAFI_CERT_ZONE"))
 
 	var notBefore time.Time
 	var err error
@@ -35,70 +34,80 @@ func (p *VenafiProvider) Provision(host string, validFrom string, validFor time.
 
 	notAfter := notBefore.Add(validFor)
 
-	username := os.Getenv("VENAFI_USER_NAME")
-	password := os.Getenv("VENAFI_PASSWORD")
-	url := os.Getenv("VENAFI_API_URL")
-	venafi := vcert.NewClientTPP(username, password, url)
-	enrollreq := &govcert.EnrollReq{
-		CommonName: host,
-		Zone:       os.Getenv("VENAFI_CERT_ZONE")}
-
-	resp, err := venafi.Do(enrollreq)
+	//
+	// 0. get client instance based on connection config
+	//
+	c, err := vcert.NewClient(tppConfig)
 	if err != nil {
-		return KeyPair{}, NewCertError("Failed to create certificate: " + err.Error())
+		t.Fatalf("could not connect to endpoint: %s", err)
 	}
-	respmap, err := resp.JSONBody()
+
+	//
+	// 1.1. compose request object
+	//
+	enrollReq := &certificate.Request{
+		Subject: pkix.Name{
+			CommonName:         host,
+			Organization:       []string{"Venafi.com"},
+			OrganizationalUnit: []string{"Integration Team"},
+			Locality:           []string{"Salt Lake"},
+			Province:           []string{"Salt Lake"},
+			Country:            []string{"US"},
+		},
+		DNSNames:       []string{host},
+		CsrOrigin:      certificate.LocalGeneratedCSR,
+		KeyType:        certificate.KeyTypeRSA,
+		KeyLength:      2048,
+		ChainOption:    certificate.ChainOptionRootLast,
+		KeyPassword:    "newPassw0rd!",
+	}
+
+	//
+	// 1.2. generate private key and certificate request (CSR) based on request's options
+	//
+	err = c.GenerateRequest(nil, enrollReq)
 	if err != nil {
-		return KeyPair{}, NewCertError("Failed to create certificate: " + err.Error())
+		t.Fatalf("could not generate certificate request: %s", err)
 	}
-	var cert, key []byte
 
-	key = []byte(respmap["PrivateKey"].(string))
-
-	if !resp.Pending() {
-		cert = []byte(respmap["Certificate"].(string))
-
-		if err != nil {
-			return KeyPair{}, NewCertError("Failed to create certificate: " + err.Error())
-		}
-		return KeyPair{
-			cert,
-			key,
-			notAfter}, nil
-	}
-	id, err := resp.RequestID()
+	//
+	// 1.3. submit certificate request, get request ID as a response
+	//
+	requestID, err := c.RequestCertificate(enrollReq, "")
 	if err != nil {
-		return KeyPair{}, NewCertError("Failed to create certificate: " + err.Error())
+		t.Fatalf("could not submit certificate request: %s", err)
 	}
-	pickupreq := &govcert.PickupReq{
-		PickupID: id,
-	}
-	retryerr := resource.Retry(time.Duration(300)*time.Second, func() *resource.RetryError {
-		resp, err = venafi.Do(pickupreq)
-		if err != nil {
-			return resource.NonRetryableError(err)
-		}
-		if resp.Pending() {
-			return resource.RetryableError(fmt.Errorf("Certificate Issue pending"))
-		}
+	t.Printf("Successfully submitted certificate request. Will pickup certificate by ID %s", requestID)
 
-		return nil
-	})
-	if retryerr != nil {
-		return KeyPair{}, NewCertError("Failed to create certificate: " + retryerr.Error())
+	//
+	// 1.4. retrieve certificate using request ID obtained on previous step, get PEM collection as a response
+	//
+	pickupReq := &certificate.Request{
+		PickupID: requestID,
+		Timeout:  180 * time.Second,
 	}
-
-	respmap, err = resp.JSONBody()
+	pcc, err := c.RetrieveCertificate(pickupReq)
 	if err != nil {
-		return KeyPair{}, NewCertError("Failed to create certificate: " + err.Error())
+		t.Fatalf("could not retrieve certificate using requestId %s: %s", requestID, err)
 	}
-	cert = []byte(respmap["Certificate"].(string))
+
+	//
+	// 1.5. (optional) add certificate's private key to PEM collection
+	//
+	pcc.AddPrivateKey(enrollReq.PrivateKey, []byte(enrollReq.KeyPassword))
+
+	t.Printf("Successfully picked up certificate for %s", host)
+	pp(pcc)
+
+	var cert = []byte(pcc.Certificate)
+	var privateKey = []byte(pcc.PrivateKey)
 
 	return KeyPair{
 		cert,
-		key,
+		privateKey,
 		notAfter}, nil
 }
+
 func (p *VenafiProvider) Deprovision(host string) error {
 	return nil
 }
