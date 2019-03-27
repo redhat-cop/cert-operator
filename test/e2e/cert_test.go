6da7588b1c377ca96c94e405ae0815319df38d94
Eric Sauer <esauer@redhat.com>
2019-03-27 10:21:05 -0400

    Adding an actual test

--- test/e2e/cert_test.go
+++ test/e2e/cert_test.go
@@ -6,9 +6,15 @@ import (
 	"testing"
 	"time"
 
+	"gotest.tools/assert"
+
 	routev1 "github.com/openshift/api/route/v1"
 	framework "github.com/operator-framework/operator-sdk/pkg/test"
+	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/wait"
 )
 
 var (
@@ -50,6 +56,9 @@ func routeBasicTest(t *testing.T, f *framework.Framework, ctx *framework.TestCtx
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "route-tls",
 			Namespace: namespace,
+			Annotations: map[string]string{
+				"openshift.io/cert-ctl-status": "new",
+			},
 		},
 		Spec: routev1.RouteSpec{
 			Host: fmt.Sprintf("route-tls.%s.example.com", namespace),
@@ -69,6 +78,10 @@ func routeBasicTest(t *testing.T, f *framework.Framework, ctx *framework.TestCtx
 		return err
 	}
 
+	err = f.Client.Get(goctx.TODO(), types.NamespacedName{Name: "route-tls", Namespace: namespace}, exampleRoute)
+
+	assert.Assert(t, waitForRouteAnnotation(t, f, namespace, "route-tls", "openshift.io/cert-ctl-status", "new", retryInterval, timeout))
+
 	return nil
 }
 
@@ -82,10 +95,46 @@ func SetupCluster(t *testing.T) {
 	}
 	t.Log("Initialized cluster resources")
 
+	namespace, err := ctx.GetNamespace()
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	// get global framework variables
 	f := framework.Global
+
+	// wait for example-memcached to reach 3 replicas
+	err = e2eutil.WaitForOperatorDeployment(t, f.KubeClient, namespace, "cert-operator", 3, retryInterval, timeout)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	if err = routeBasicTest(t, f, ctx); err != nil {
 		t.Fatal(err)
 	}
+}
+
+func waitForRouteAnnotation(t *testing.T, f *framework.Framework, namespace string, name string, annotation string, expectedValue string, retryInterval time.Duration, timeout time.Duration) error {
+	err := wait.Poll(retryInterval, timeout, func() (done bool, err error) {
+		route := &routev1.Route{}
+		err = f.Client.Get(goctx.TODO(), types.NamespacedName{Name: "route-tls", Namespace: namespace}, route)
+		if err != nil {
+			if apierrors.IsNotFound(err) {
+				t.Logf("Waiting for availability of %s deployment\n", name)
+				return false, nil
+			}
+			return false, err
+		}
+
+		if route.ObjectMeta.Annotations[annotation] == expectedValue {
+			return true, nil
+		}
+		t.Logf("Waiting for operator to reconcile Route %s (current %s; want %s)\n", name, route.ObjectMeta.Annotations[annotation], expectedValue)
+		return false, nil
+	})
+	if err != nil {
+		return err
+	}
+	t.Logf("Route reconciled! %s\n", name)
+	return nil
 }
